118e577a2c6c2691e47fd3d24ca7253bd1fd39c0
SANDEEP MALVIYA <34533044+sandeepmalviya581@users.noreply.github.com>
2023-11-30 19:03:05 +0530

    toaster in exam result

--- src/app/examportal/result/ExamResultsTable.jsx
+++ src/app/examportal/result/ExamResultsTable.jsx
@@ -2,6 +2,7 @@
 "use client"
 import { getAllStudentResultAPI } from '@/services/examportalService';
 import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 
 const ExamResultsTable = () => {
 
@@ -11,7 +12,9 @@ const ExamResultsTable = () => {
         try {
             const res = await getAllStudentResultAPI();
             setResults(res);
+            toast.success("Result fetched successfully.")
         } catch (error) {
+            toast.success("Failed to fetch result.")
             console.log(error);
         }
     }
@@ -22,11 +25,11 @@ const ExamResultsTable = () => {
 
     return (
 
-        <div className="container mx-auto mt-8 p-4">
+        <div className="mx-auto mt-8 p-4">
             <h1 className="text-3xl font-bold mb-4">Exam Results</h1>
 
             <button
-                onClick={() => getAllStudentResult}
+                onClick={() => getAllStudentResult()}
                 className={`bg-blue-500 hover:bg-blue-70 mt-2 text-white font-bold py-2 px-4 rounded-full`}
             >
                 Refresh
@@ -38,7 +41,6 @@ const ExamResultsTable = () => {
                     <thead className="bg-gray-100">
                         <tr>
                             <th className="py-2 px-4 text-left font-semibold text-gray-700">Student Name</th>
-                            <th className="py-2 px-4 text-left font-semibold text-gray-700">Email</th>
                             {/* <th className="py-2 px-4 text-left font-semibold text-gray-700">ID</th> */}
                             <th className="py-2 px-4 text-left font-semibold text-gray-700">Total Question</th>
                             <th className="py-2 px-4 text-left font-semibold text-gray-700">Attempt</th>
@@ -50,13 +52,14 @@ const ExamResultsTable = () => {
 
                             <th className="py-2 px-4 text-left font-semibold text-gray-700">Final Obtained Marks</th>
                             <th className="py-2 px-4 text-left font-semibold text-gray-700">Rank</th>
+                            <th className="py-2 px-4 text-left font-semibold text-gray-700">Email</th>
+
                         </tr>
                     </thead>
                     <tbody className="bg-white">
                         {results.map((result, index) => (
                             <tr key={index} className={index % 2 === 0 ? 'bg-gray-50' : 'bg-white'}>
                                 <td className="py-3 px-4 text-gray-800">{result.fullName}</td>
-                                <td className="py-3 px-4 text-gray-800">{result.email}</td>
                                 {/* <td className="py-3 px-4 text-gray-800">{result.userId}</td> */}
                                 <td className="py-3 px-4 text-gray-800">{result.totalQuestion}</td>
                                 <td className="py-3 px-4 text-gray-800">{result.attemptQuestion}</td>
@@ -68,6 +71,8 @@ const ExamResultsTable = () => {
 
                                 <td className="py-3 px-4 text-gray-800">{result.userMarks}</td>
                                 <td className="py-3 px-4 text-gray-800">{index++ + 1}</td>
+                                <td className="py-3 px-4 text-gray-800">{result.email}</td>
+
                             </tr>
                         ))}
                     </tbody>
